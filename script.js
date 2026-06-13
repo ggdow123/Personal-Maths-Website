a1ea132ec8e3d5388cd87879d04d93bed5b6133a
ggdow123 <ggdow12345@outlook.com>
2026-06-13 22:46:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Biography site loaded successfully.");
 });
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 120;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop &&
+            pageYOffset < sectionTop + sectionHeight) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
